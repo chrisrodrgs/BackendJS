65a7644711797f54f1e9f22f10a97627873b96ea
chrisrodrgs <christianrodrgs@gmail.com>
2025-10-02 09:22:20 -0300

    Sites que estou desenvolvendo para estudos.

---
+++ Aula01/letConst.js
@@ -0,0 +1,14 @@
+//declaração de variável
+
+let nome = "Christian";
+
+//Declaração de constante
+const sobrenome = "Rodrigues"
+
+console.log(nome, sobrenome);
+//o var, pode acessar qualquer linha do arquivo. o let precisa que a linha seja escrita antes da leitura do console.log
+
+
+var nomevar = "Christian";
+
+// Var, let e const:}
