f11f584c5284feda52bdef0a426717b45e09414c
chrisrodrgs <christianrodrgs@gmail.com>
2025-10-01 22:02:55 -0300

    exercicio aula 1 js

---
+++ Aula01/exercicio1.js
@@ -0,0 +1,10 @@
+const nome = "Diego"
+const sobrenome = "Rodrigues";
+const idade = 39;
+const peso = 80;
+const altura = 1.72;
+
+const imc = peso / (altura * altura);
+
+console.log(nome+' '+sobrenome+' tem '+ idade +' anos, pesa '+peso+'kg. Tem '+altura+'m de altura e seu IMC é de '+imc)
+
---
+++ Aula01/index.js
@@ -0,0 +1,8 @@
+// Meu primeiro código
+console.log('Olá,mundo!');
+console.log("Olá, mundo com aspas duplas");
+console.log('Olá, mundo com aspas duplas');
+/*
+Tudo que estiver aqui será ingoado pelo código.
+Não importa a quantidade de linhas
+*/
